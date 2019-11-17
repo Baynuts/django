49bdd4400e0123a8aa31b21501b6df6051c8fa15
D Bailey <github@baileygb.com>
2019-11-17 20:42:24 +0000

    Setup Templates

--- django_todo/urls.py
+++ django_todo/urls.py
@@ -13,9 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls import url
 from django.contrib import admin
-from django.urls import path
+from todo.views import get_todo_list
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    url(r'^admin/', admin.site.urls),
+    url(r'^todo$', get_todo_list)
 ]
