9b7c9d851fe122142e6af7a8444da43ce12920a2
D Bailey <github@baileygb.com>
2019-11-21 19:49:22 +0000

    Item form setup

--- todo/views.py
+++ todo/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, redirect
+from .models import Item
+from .forms import ItemForm
 
 # Create your views here.
 def say_hello(request):
@@ -6,4 +8,16 @@ def say_hello(request):
 
 
 def get_todo_list(request):
-    return render(request, "todo_list.html")
+    results = Item.objects.all()
+    return render(request, "todo_list.html", {'items':results } )
+
+def create_an_item(request):
+    if request.method=="POST":
+        form = ItemForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect(get_todo_list)
+    else:     
+        form = ItemForm()
+
+    return render(request, "item_form.html", {'form': form})
