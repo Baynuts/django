7628382611b8a9bf4dd408f04758bb4ec1250bf7
D Bailey <github@baileygb.com>
2019-12-01 11:43:20 +0000

    Set up Postgres database

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -74,12 +75,15 @@ WSGI_APPLICATION = 'django_todo.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+#}
+
+DATABASES = {'default':dj_database_url.parse("postgres://shmxvvmoxdrgvf:2b778d6ca85002c3dc980280bdfae3d3fb107b11e5ea8c14884c44aee0f21b5d@ec2-46-137-187-23.eu-west-1.compute.amazonaws.com:5432/d20g9v5ftfes2h")}
+
 
 
 # Password validation
